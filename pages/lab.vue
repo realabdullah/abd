b77a32b27febc4e862a81874f4ab71cd17bc60b1
realabdullah <abdulodesanmi@gmail.com>
2025-02-23 09:45:00 +0100

    fix(lab): wrap ContentQuery in a template for conditional rendering

--- pages/lab.vue
+++ pages/lab.vue
@@ -3,17 +3,19 @@
     <AppHeader class="mb-12" title="Lab" :description="description" />
     <div class="space-y-24">
       <ContentList path="/lab" v-slot="{ list }">
-        <ContentQuery
-          v-for="item in list"
-          :key="item._path"
-          :path="item._path"
-          find="one"
-          v-slot="{ data }"
-        >
-          <ContentRenderer>
-            <ContentRendererMarkdown :value="data" />
-          </ContentRenderer>
-        </ContentQuery>
+        <template v-if="list">
+          <ContentQuery
+            v-for="item in list"
+            :key="item._path"
+            :path="item._path"
+            find="one"
+            v-slot="{ data }"
+          >
+            <ContentRenderer>
+              <ContentRendererMarkdown :value="data" />
+            </ContentRenderer>
+          </ContentQuery>
+        </template>
       </ContentList>
     </div>
   </main>
