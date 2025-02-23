bedea6cf2d80be3239223e68a8267ac76edcf4fa
realabdullah <abdulodesanmi@gmail.com>
2025-02-23 08:51:08 +0100

    (fix): failing deployments

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,15 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: [
     "@nuxt/ui",
-    "nuxt-icon",
-    "@nuxtjs/google-fonts",
-    "@nuxtjs/fontaine",
     "@nuxt/image",
     "@nuxt/content",
-    "@nuxthq/studio",
-    "@vueuse/nuxt"
+    "@vueuse/nuxt",
+    "@nuxt/icon",
+    "@nuxt/fonts",
   ],
-  ui: {
-    icons: ["heroicons", "lucide"],
-  },
+
   app: {
     pageTransition: { name: "page", mode: "out-in" },
     head: {
@@ -25,15 +22,6 @@ export default defineNuxtConfig({
       },
     },
   },
-  content: {
-    highlight: {
-      theme: "github-dark",
-    },
-  },
-  googleFonts: {
-    display: "swap",
-    families: {
-      Inter: [400, 500, 600, 700, 800, 900],
-    },
-  },
+
+  compatibilityDate: "2025-02-23"
 });
